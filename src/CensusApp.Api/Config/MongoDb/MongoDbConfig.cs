274a9848f9592069bd8455b6e7f4fcc1314804cc
lano.castro <lano.castro@cg.df.gov.br>
2022-03-01 20:50:41 -0300

    ReAdiciona lib MongoDb e registra suas configurações no Startup

---
+++ src/CensusApp.Api/Config/MongoDb/MongoDbConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace CensusApp.Api.Config.MongoDb
+{
+    public class MongoDbConfig
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/CensusApp.Api/Config/MongoDb/MongoDbConfigurationExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System;
+
+namespace CensusApp.Api.Config.MongoDb
+{
+    public static class MongoDbConfigurationExtension
+    {
+        public static void AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoConfigSection = configuration.GetSection("MongoDb");
+            var mongoDbConfig = mongoConfigSection.Get<MongoDbConfig>();
+
+            services.AddSingleton<IMongoClient>(new MongoClient(new MongoClientSettings()
+            {
+                ConnectTimeout = TimeSpan.FromSeconds(10),
+                Server = new MongoServerAddress(mongoDbConfig.Server, mongoDbConfig.Port),
+                Credential = MongoCredential.CreateCredential(mongoDbConfig.Database, mongoDbConfig.Username, mongoDbConfig.Password)
+            }));
+
+            services.AddSingleton<IMongoDatabase>(scope => scope.GetService<IMongoClient>().GetDatabase(mongoDbConfig.Database));
+        }
+    }
+}
