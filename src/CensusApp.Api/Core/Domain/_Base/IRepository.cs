472c77741b19d6f1b603c59261b4d73163c0e1a5
lano.castro <lano.castro@cg.df.gov.br>
2022-03-01 19:33:57 -0300

    implementação do pattern Repository com MongoDb

---
+++ src/CensusApp.Api/Core/Domain/_Base/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace CensusApp.Api.Core.Domain._Base
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        void Add(TEntity obj);
+        Task AddAsync(TEntity obj);
+        void Update(TEntity obj);
+        Task UpdateAsync(TEntity obj);
+        void Delete(TEntity obj);
+        TEntity Get(object id);
+
+    }
+}
---
+++ src/CensusApp.Api/Core/Infra/Data/MongoDbRepositoryt.cs
@@ -0,0 +1,52 @@
+﻿using CensusApp.Api.Core.Domain._Base;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CensusApp.Api.Core.Infra.Data
+{
+    public class MongoDbRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        private readonly IMongoCollection<TEntity> _collection;
+        public MongoDbRepository(IMongoDatabase mongoDatabase)
+        {
+            _collection = mongoDatabase.GetCollection<TEntity>(GetCollectionName(typeof(TEntity)));
+        }
+        public void Add(TEntity obj)
+        {
+            _collection.InsertOne(obj);
+        }
+        public Task AddAsync(TEntity obj)
+        {
+            return _collection.InsertOneAsync(obj);
+        }
+        public void Delete(TEntity obj)
+        {
+            obj.Delete();
+            Update(obj);
+        }
+        public void Update(TEntity obj)
+        {
+            _collection.ReplaceOne(FilterById(obj.Id), obj);
+        }
+        public Task UpdateAsync(TEntity obj)
+        {
+            return _collection.ReplaceOneAsync(FilterById(obj.Id), obj);
+        }
+        public TEntity Get(object id)
+        {
+            return _collection.Find(FilterById(id)).FirstOrDefault();
+        }
+        private string GetCollectionName(Type documentType)
+        {
+            return ((BsonCollectionAttribute)documentType.GetCustomAttributes(
+                    typeof(BsonCollectionAttribute), true)
+                .FirstOrDefault())?.CollectionName;
+        }
+        private FilterDefinition<TEntity> FilterById(object id)
+        {
+            return Builders<TEntity>.Filter.Eq(doc => doc.Id, id);
+        }
+    }
+}
