a277b67ab03f895d3b59cc683bd77c8ab103e544
lano.castro <lano.castro@cg.df.gov.br>
2022-03-06 21:46:14 -0300

    implementação de agrupamento de dados para dashboard

---
+++ src/CensusApp.Api/Controllers/DashboardController.cs
@@ -0,0 +1,51 @@
+﻿using CensusApp.Api.Core.Infra.Data.Queries;
+using CensusApp.Api.Core.Infra.Data.Queries.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CensusApp.Api.Controllers
+{
+    [Route("api/dashboard")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetData()
+        {
+            var query = new ConsultarPessoa();
+            var response = await _mediator.Send(query);
+
+            var data = new
+            {
+                regiao = await Select(response.GroupBy(x => x.Regiao)),
+                escolaridade = await Select(response.GroupBy(x => x.Escolaridade)),
+                racaCor = await Select(response.GroupBy(x => x.RacaCor)),
+                total = response.Count()
+            };
+
+            return Ok(data);
+
+        }
+
+        private async Task<object> Select(IEnumerable<IGrouping<string, PessoaViewModel>> group)
+        {
+            return group.Select(x => new
+            {
+                label=x.Key,
+                value=x.Count()
+
+            });           
+        }
+    }
+
+}
